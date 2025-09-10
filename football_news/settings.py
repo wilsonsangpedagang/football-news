d4096442d1c839137626ac63e2f431bc226b1dd5
davin <davinmagic3@gmail.com>
2025-09-10 14:52:29 +0700

    Finish tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,6 +33,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","davin-muhammad-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://davin-muhammad-footballnews.pbp.cs.ui.ac.id/"
+]
 
 # Application definition
 
@@ -61,7 +64,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']    ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
