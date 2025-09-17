90ae424c18c069f7d2a984596abbf4f617c1926d
davin <davinmagic3@gmail.com>
2025-09-17 13:40:52 +0700

    Tutorial 3 Finish

--- main/models.py
+++ main/models.py
@@ -1,7 +1,10 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class News(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) # add this
+
     CATEGORY_CHOICES = [
         ('transfer', 'Transfer'),
         ('update', 'Update'),
--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from main.views import show_main, create_news, show_news
 from main.views import show_main, create_news, show_xml, show_json
 from main.views import show_main, create_news, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
 
 app_name = 'main'
 
@@ -13,6 +16,9 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
 
 
